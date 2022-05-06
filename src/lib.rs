d004e62b26a187f5d425cc2f0b36f664365ca97e
s4110 <shonborilion12@gmail.com>
2022-05-06 13:15:13 +0900

    wip: up to cpu-exceptions

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,12 @@
 #![no_std]
 #![cfg_attr(test, no_main)]
-#![feature(custom_test_frameworks)]
+#![feature(custom_test_frameworks, abi_x86_interrupt)]
 #![test_runner(crate::test_runner)]
 #![reexport_test_harness_main = "test_main"]
 
 pub mod serial;
 pub mod vga_buffer;
+pub mod interrupts;
 
 use core::panic::PanicInfo;
 
@@ -58,6 +59,7 @@ pub fn test_panic_handler(info: &PanicInfo) -> ! {
 #[cfg(test)]
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
+    init();
     test_main();
     loop {}
 }
@@ -68,3 +70,6 @@ fn panic(info: &PanicInfo) -> ! {
     test_panic_handler(info)
 }
 
+pub fn init() {
+    interrupts::init_idt();
+}
